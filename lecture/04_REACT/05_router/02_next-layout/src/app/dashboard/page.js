1cb21e0e0b8c2b9643a6c46cc72e4cad7c579b0b
HaewonJeong <bonjour_haewon@naver.com>
2026-04-26 23:00:07 +0900

    SpotifyMini 일지 저장

--- lecture/04_REACT/05_router/02_next-layout/src/app/dashboard/page.js
+++ lecture/04_REACT/05_router/02_next-layout/src/app/dashboard/page.js
@@ -1,8 +1,8 @@
 export default function Dashboard(){
     return(
-        <>
+        <>=app/dashboard/page.js=
             <h1>대시보드입니다.</h1>
-            <p>여기는 관리자 페이지 입니다.</p>
+            <p>여기는 관리자 페이지 입니다.</p>=app/dashboard/page.js=
         </>
     )
 }
--- lecture/04_REACT/05_router/05_zustand/01_zustand-basic/src/components/GrandChild.js
+++ lecture/04_REACT/05_router/05_zustand/01_zustand-basic/src/components/GrandChild.js
@@ -5,6 +5,7 @@ export default function GrandChild(){
 
     const {count, increase, decrease, text, setText } = useStore();
     console.log(count);
+    
 
     return (
         <div>
@@ -22,3 +23,42 @@ export default function GrandChild(){
         </div>
     )
 }
+
+//===[람다식, 구조분해 사용 X]===
+// import { useStore } from "@/store/useStore";
+
+// export default function GrandChild() {
+//     // 1. 구조 분해를 하지 않고 변수에 전체 스토어를 담습니다.
+//     const store = useStore();
+    
+//     // 2. 개별 변수에 점 표기법(Dot Notation)으로 하나씩 할당합니다.
+//     const count = store.count;
+//     const increase = store.increase;
+//     const decrease = store.decrease;
+//     const text = store.text;
+//     const setText = store.setText;
+
+//     console.log(count);
+
+//     // 3. 이벤트 핸들러도 일반 함수로 작성
+//     function handleChange(e) {
+//         setText(e.target.value);
+//     }
+
+//     return (
+//         <div>
+//             <h2>{count}</h2>
+//             {/* 할당된 변수를 그대로 사용하거나 store.increase처럼 직접 써도 됩니다. */}
+//             <button onClick={increase}>+1</button>
+//             <button onClick={decrease}>-1</button>
+
+//             <hr />
+
+//             <input
+//                 value={text}
+//                 onChange={handleChange} 
+//             />
+//             <p>{text}</p>
+//         </div>
+//     );
+// }
